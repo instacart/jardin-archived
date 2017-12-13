f5bb46a62c475a5352b1db1847abf2c9a876dff3
Emmanuel Turlay <emmanuel@instacart.com>
2017-12-13 15:51:26 -0800

    Do not attempt to insert if no values

--- jardin/model.py
+++ jardin/model.py
@@ -1,10 +1,12 @@
-from database import DatabaseAdapter, DatabaseConnections
 from datetime import datetime
-from record import Record
 import pandas as pd
 import numpy as np
 import re, inspect
 import os
+
+import config
+from record import Record
+from database import DatabaseAdapter, DatabaseConnections
 
 
 class Model(pd.DataFrame):
@@ -159,6 +161,9 @@ class Model(pd.DataFrame):
         :param values: A dictionary containing the values to be inserted. ``datetime``, ``dict`` and ``bool`` objects can be passed as is and will be correctly serialized by psycopg2.
         :type values: dict
         """
+        if len(kwargs['values']) == 0:
+            config.logger.warning('No values to insert.')
+            return
         kwargs['stack'] = self.stack_mark(inspect.stack())
         kwargs['primary_key'] = self.record_class.primary_key
         column_names = self.column_names()
--- tests/test_jardin.py
+++ tests/test_jardin.py
@@ -1,4 +1,6 @@
 import unittest
+import pandas as pd
+
 from models import JardinTestModel
 
 
@@ -7,28 +9,33 @@ class Users(JardinTestModel): pass
 
 class TestTransaction(object):
 
-    def __init__(self, model):
+    def __init__(self, model, create_table=True):
         self._model = model
         self._connection = self._model.db(role='master')
+        self.create_table = create_table
 
     def __enter__(self):
-        self._model.query(sql='drop table if exists %s cascade;' % self._model.model_metadata()['table_name'])
+        self._model.query(
+            sql='drop table if exists %s cascade;' % self._model.model_metadata()['table_name']
+            )
         self._model._columns = None
         self._connection.autocommit = False
         self._model.query(sql='BEGIN;', role='master')
+        if self.create_table:
+            self._model.query(
+                sql='CREATE TABLE users (id serial PRIMARY KEY, name varchar(256), created_at timestamp, updated_at timestamp);'
+                )
+
 
     def __exit__(self, exc_type, exc_value, traceback):
         self._model.query(sql='ROLLBACK;', role='master')
         self._connection.autocommit = True
 
 
-
 class TestModel(unittest.TestCase):
+
     def test_created_at_updated_at(self):
         with TestTransaction(Users):
-            Users.query(
-                sql='CREATE TABLE users (id serial PRIMARY KEY, name varchar(256), created_at timestamp, updated_at timestamp);'
-                )
             user = Users.insert(values={'name': 'Jardinier'})
             self.assertIsNotNone(user.created_at)
             self.assertIsNotNone(user.updated_at)
@@ -40,7 +47,7 @@ class TestModel(unittest.TestCase):
             self.assertEqual(updated_user.created_at, user.created_at)
 
     def test_no_created_at_updated_at(self):
-        with TestTransaction(Users):
+        with TestTransaction(Users, create_table=False):
             Users.query(
                 sql='CREATE TABLE users (id serial PRIMARY KEY, name varchar(256));'
                 )
@@ -48,3 +55,14 @@ class TestModel(unittest.TestCase):
             self.assertEqual(Users.count(), 1)
             self.assertFalse('updated_at' in user)
             self.assertFalse('created_at' in user)
+
+    def test_empty_insert(self):
+        with TestTransaction(Users):
+            Users.insert(values={})
+            self.assertEqual(Users.count(), 0)
+            Users.insert(values=pd.DataFrame())
+            self.assertEqual(Users.count(), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
