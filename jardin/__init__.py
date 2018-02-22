151a5b88f5f41e74be0153514e78c5ae885f95b5
Emmanuel Turlay <emmanuel@instacart.com>
2018-02-22 14:07:52 -0800

    nastier fix

--- jardin/__init__.py
+++ jardin/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-if not sys.jardin_setup:
+if not hasattr(sys, 'jardin_setup') or (hasattr(sys, 'jardin_setup') and not sys.jardin_setup):
     from jardin.model import Model, Collection
     import jardin.config
 
@@ -8,7 +8,7 @@ __author__ = 'Emmanuel Turlay'
 __copyright__ = 'Copyright 2017, Instacart'
 __credits__ = ['Emmanuel Turlay', 'Mathieu Ripert']
 __license__ = 'MIT'
-__version__ = '0.14.1'
+__version__ = '0.14.2'
 __maintainer__ = 'Emmanuel Turlay'
 __email__ = 'emmanuel@instacart.com'
 __status__ = 'Prototype'
