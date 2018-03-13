da5d84e31a85487cf98e2f19fa04e2840a3324a7
Emmanuel Turlay <emmanuel@instacart.com>
2018-03-13 16:51:05 -0700

    attempt to fix build

--- jardin/database/mysql.py
+++ jardin/database/mysql.py
@@ -12,10 +12,10 @@ class DatabaseConnection(BaseConnection):
 
     DRIVER = MySQLdb
 
-    @retry(MySQLdb.OperationalError, tries=3)
+    @retry(DRIVER.OperationalError, tries=3)
     def connect(self):
         return super(DatabaseConnection, self).connect()
 
-    @retry(MySQLdb.InterfaceError, tries=3)
+    @retry(DRIVER.InterfaceError, tries=3)
     def execute(self, *query):
         return super(DatabaseConnection, self).execute(*query)
