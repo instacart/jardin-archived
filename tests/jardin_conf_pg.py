b1e3f5616c2d0bec3a1e048ccb50420cf8f81d7e
Mathieu Ripert <mathieu.ripert@gmail.com>
2019-07-29 15:12:44 +0200

    fix issue #45

--- tests/jardin_conf_pg.py
+++ tests/jardin_conf_pg.py
@@ -4,7 +4,14 @@ import os
 PGPORT = os.environ.get("PGPORT", 5432)
 
 DATABASES = {
-    'jardin_test': 'postgres://postgres:@localhost:%s/jardin_test' % PGPORT
+    'jardin_test': 'postgres://postgres:@localhost:%s/jardin_test' % PGPORT,
+    'other_test_dict_config': {
+        'username': 'test',
+        'password': 'test',
+        'database': 'jardin_test',
+        'host': 'localhost',
+        'port': 1234
+    }
 }
 
 LOG_LEVEL = logging.INFO
