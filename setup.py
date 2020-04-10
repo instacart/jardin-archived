0292c97ac88ee80158b99ddf17d63421cb372136
Mathieu Ripert <mathieu.ripert@gmail.com>
2020-04-10 15:08:57 +0200

    do not support 2.7 anymore

--- setup.py
+++ setup.py
@@ -31,12 +31,11 @@ setup(
       'inflect',
       'future'
       ],
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+      python_requires='>=3.5, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
       test_suite='tests',
       classifiers = [
       'Intended Audience :: Developers',
       'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python :: 2.7',
       'Programming Language :: Python :: 2',
       'Programming Language :: Python :: 3',
       'Programming Language :: Python :: 3.5',
