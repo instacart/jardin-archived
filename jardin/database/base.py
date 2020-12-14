instacart/jardin-archived

commit 80671e28904f6ade0b7e38b798a2aebfbd032ccf

Connections Pool with Postgres (#57)
* Connection Pooling

* bump version

* test with connection pool

* add python 3.7.8 for tests

* check if connection pools is present

* fix tests

* Update getting_started.rst

* readme

* use cursor as context manager

* fix when no results returned

* fix for mysql

* fix with sqlite

* transaction with connection pool

* python 3.5.6 minimum

* handle no pool name

* fix update method returning None

* fix tests

* fix connection pool

* fix tests

* fix tests

jardin/database/base.py
@@ -1,4 +1,6 @@
 from memoized_property import memoized_property
+from contextlib import contextmanager
+import threading
 
 
 class BaseConnection(object):
@@ -7,18 +9,38 @@ class BaseConnection(object):
     LEXICON = None
 
     _connection = None
-    _cursor = None
 
-    def __init__(self, db_config, name):
+    def __init__(self, db_config, name, pool_config=None):
         self.db_config = db_config
         self.autocommit = True
         self.name = name
         self.lexicon = self.LEXICON()
+        self.pool_config = pool_config
 
+    @contextmanager
     def connection(self):
-        if self._connection is None:
-            self._connection = self.connect()
-        return self._connection
+        try:
+            conn = self.get_connection()
+            yield conn
+            if self.autocommit:
+                conn.commit()
+        except Exception as e:
+            self.rollback()
+        finally:
+            if self.pool is not None and self.autocommit:
+                key = threading.current_thread().ident
+                self.pool.putconn(conn, key=key)
+
+    def commit(self):
+        conn = self.get_connection()
+        conn.commit()
+        if self.pool:
+            key = threading.current_thread().ident
+            self.pool.putconn(conn, key=key)
+
+    def rollback(self):
+        conn = self.get_connection()
+        conn.rollback()
 
     @memoized_property
     def connect_kwargs(self):
@@ -29,35 +51,43 @@ class BaseConnection(object):
             host=self.db_config.host,
             port=self.db_config.port,
             connect_timeout=5
-            )
+        )
 
     @memoized_property
     def connect_args(self):
         return []
 
-    def connect(self):
-        self._cursor = None
-        return self.DRIVER.connect(*self.connect_args, **self.connect_kwargs)
+    @memoized_property
+    def pool(self):
+        return None
+
+    def get_connection(self):
+        if self.pool is None:
+            if self._connection is None:
+                self._connection = self.DRIVER.connect(*self.connect_args, **self.connect_kwargs)
+            return self._connection
+        key = threading.current_thread().ident
+        return self.pool.getconn(key=key)
 
     @memoized_property
     def cursor_kwargs(self):
         return {}
 
-    def cursor(self):
-        if self._cursor is None:
-            self._cursor = self.connection().cursor(**self.cursor_kwargs)
-        return self._cursor
+    def columns(self, cursor):
+        cursor_desc = cursor.description
+        columns = []
+        if cursor_desc:
+            columns = [col_desc[0] for col_desc in cursor_desc]
+            if self.db_config.lowercase_columns:
+                columns = [col.lower() for col in columns]
+        return columns
 
-    def execute(self, *query):
-        try:
-            results = self.cursor().execute(*query)
-            if self.autocommit:
-                self.connection().commit()
-            return results
-        except self.DRIVER.InterfaceError:
-            self._connection = None
-            self._cursor = None
-            raise
-        except Exception as e:
-            self.connection().rollback()
-            raise e
+    def execute(self, *query, write=False, **kwargs):
+        with self.connection() as connection:
+            cursor = connection.cursor(**self.cursor_kwargs)
+            cursor.execute(*query)
+            if write:
+                return self.lexicon.row_ids(cursor, kwargs['primary_key'])
+            if cursor.description:
+                return cursor.fetchall(), self.columns(cursor)
+            return None, None

.travis.yml
@@ -1,11 +1,13 @@
 language: python
 python:
-  - 3.5.2
+  - 3.5.6
   - 3.6.4
+  - 3.7.8
 script:
   - JARDIN_CONF=/home/travis/build/instacart/jardin/tests/jardin_conf_pg.py python setup.py test
   - JARDIN_CONF=/home/travis/build/instacart/jardin/tests/jardin_conf_mysql.py python setup.py test
   - JARDIN_CONF=/home/travis/build/instacart/jardin/tests/jardin_conf_sqlite.py python setup.py test
+  - JARDIN_CONF=/home/travis/build/instacart/jardin/tests/jardin_conf_pg_with_connection_pool.py python setup.py test
 services:
   - postgresql
   - mysql

jardin/__init__.py
@@ -11,7 +11,7 @@ __author__ = 'Emmanuel Turlay'
 __copyright__ = 'Copyright 2017, Instacart'
 __credits__ = ['Emmanuel Turlay', 'Mathieu Ripert']
 __license__ = 'MIT'
-__version__ = '0.19.12'
+__version__ = '0.20.0'
 __maintainer__ = 'Emmanuel Turlay'
 __email__ = 'emmanuel@instacart.com'
 __status__ = 'Prototype'

jardin/config.py
@@ -8,11 +8,12 @@ def init():
     if INITIALIZED:
         return
     
-    global DATABASES, WATERMARK, LOG_LEVEL, logger
+    global DATABASES, CONNECTION_POOLS, WATERMARK, LOG_LEVEL, logger
     
     config_file = imp.load_source('jardin_conf', os.environ.get('JARDIN_CONF', 'jardin_conf.py'))
     
     DATABASES = config_file.DATABASES
+    CONNECTION_POOLS = config_file.CONNECTION_POOLS if getattr(config_file, 'CONNECTION_POOLS', None) else {}
     
     this = sys.modules[__name__]
     

jardin/database/__init__.py
@@ -54,7 +54,8 @@ class DatabaseConnections(object):
                 import jardin.database.drivers.sqlite as driver
             elif db.scheme == 'snowflake':
                 import jardin.database.drivers.sf as driver
-            connections.append(driver.DatabaseConnection(db, name))
+            pool_config = config.CONNECTION_POOLS.get(name, None)
+            connections.append(driver.DatabaseConnection(db, name, pool_config=pool_config))
         return connections
 
     @classmethod
@@ -111,20 +112,19 @@ class DatabaseAdapter(object):
     def select(self, **kwargs):
         query = SelectQueryBuilder(**kwargs).query
         config.logger.debug(query)
-        self.db.execute(*query)
-        results = self.db.cursor().fetchall()
-        return pandas.DataFrame.from_records(list(results), columns=self.columns(), coerce_float=True)
+        results, columns = self.db.execute(*query, write=False)
+        if results is None and columns is None:
+            return None
+        return pandas.DataFrame.from_records(results, columns=columns, coerce_float=True)
 
     @set_defaults
     def write(self, query_builder, **kwargs):
         query = query_builder(**kwargs).query
         config.logger.debug(query)
-        self.db.execute(*query)
-        row_ids = self.db.lexicon.row_ids(self.db, kwargs['primary_key'])
-        if len(row_ids) > 0:
-            return self.select(where={kwargs['primary_key']: row_ids})
-
-        return pandas.DataFrame(columns=self.columns())
+        returning_ids = self.db.execute(*query, write=True, **kwargs)
+        if len(returning_ids) > 0:
+            return self.select(where={kwargs['primary_key']: returning_ids})
+        return None
 
     def insert(self, **kwargs):
         return self.write(InsertQueryBuilder, **kwargs)
@@ -136,24 +136,13 @@ class DatabaseAdapter(object):
     def delete(self, **kwargs):
         query = DeleteQueryBuilder(**kwargs).query
         config.logger.debug(query)
-        self.db.execute(*query)
+        self.db.execute(*query, write=False)
 
     @set_defaults
     def raw_query(self, **kwargs):
         query = RawQueryBuilder(**kwargs).query
         config.logger.debug(query)
-        self.db.execute(*query)
-        if self.db.cursor().description:
-            results = self.db.cursor().fetchall()
-            return pandas.DataFrame.from_records(list(results), columns=self.columns(), coerce_float=True)
-        else:
+        results, columns = self.db.execute(*query, write=False)
+        if results is None and columns is None:
             return None
-
-    def columns(self):
-        cursor_desc = self.db.cursor().description
-        columns = []
-        if cursor_desc:
-            columns = [col_desc[0] for col_desc in cursor_desc]
-            if self.db.db_config.lowercase_columns:
-                columns = [col.lower() for col in columns]
-        return columns
+        return pandas.DataFrame.from_records(results, columns=columns, coerce_float=True)

jardin/database/drivers/mysql.py
@@ -20,9 +20,9 @@ class Lexicon(BaseLexicon):
         return row['Field'], row['Default'], row['Type']
 
     @staticmethod
-    def row_ids(db, primary_key):
-        db.execute('SELECT LAST_INSERT_ID();')
-        return [db.cursor().fetchall()[0][0]]
+    def row_ids(cursor, primary_key):
+        cursor.execute('SELECT LAST_INSERT_ID();')
+        return [cursor.fetchall()[0][0]]
 
     @staticmethod
     def apply_watermark(query, watermark):
@@ -35,9 +35,10 @@ class DatabaseConnection(BaseConnection):
     LEXICON = Lexicon
 
     @retry(DRIVER.OperationalError, tries=3)
-    def connect(self):
-        return super(DatabaseConnection, self).connect()
+    def get_connection(self):
+        return super(DatabaseConnection, self).get_connection()
 
     @retry(DRIVER.InterfaceError, tries=3)
-    def execute(self, *query):
-        return super(DatabaseConnection, self).execute(*query)
+    def execute(self, *query, write=False, **kwargs):
+        return super(DatabaseConnection, self).execute(*query, write=write, **kwargs)
+

jardin/database/drivers/pg.py
@@ -1,4 +1,6 @@
+import sys
 import psycopg2 as pg
+from psycopg2.pool import SimpleConnectionPool, ThreadedConnectionPool
 from psycopg2 import extras
 from memoized_property import memoized_property
 
@@ -14,7 +16,7 @@ class Lexicon(BaseLexicon):
     def table_schema_query(table_name):
         return "SELECT column_name, column_default, data_type FROM " \
             "information_schema.columns WHERE " \
-            "table_name=%(table_name)s AND table_schema='public';" 
+            "table_name=%(table_name)s AND table_schema='public';"
 
     @staticmethod
     def column_info(row):
@@ -23,21 +25,22 @@ class Lexicon(BaseLexicon):
     @staticmethod
     def update_values(fields, value_extrapolators):
         if len(fields) == 1:
-            result = ', '.join(fields) + ' = ' + [', '.join(ext) for ext in value_extrapolators][0]
+            result = ', '.join(fields) + ' = ' + \
+                [', '.join(ext)
+                 for ext in value_extrapolators][0]
         else:
             result = '(' \
                 + ', '.join(fields) \
                 + ') = '
             result += ', '.join(
                 ['(' + ', '.join(ext) + ')' for ext in value_extrapolators]
-                )
+            )
         return result
 
     @staticmethod
-    def row_ids(db, primary_key):
-        row_ids = db.cursor().fetchall()
-        row_ids = [r[primary_key] for r in row_ids]
-        return row_ids
+    def row_ids(cursor, primary_key):
+        row_ids = cursor.fetchall()
+        return [r[primary_key] for r in row_ids]
 
 
 class DatabaseConnection(BaseConnection):
@@ -46,8 +49,8 @@ class DatabaseConnection(BaseConnection):
     LEXICON = Lexicon
 
     @retry(pg.OperationalError, tries=3)
-    def connect(self):
-        connection = super(DatabaseConnection, self).connect()
+    def get_connection(self):
+        connection = super(DatabaseConnection, self).get_connection()
         connection.initialize(config.logger)
         return connection
 
@@ -61,12 +64,18 @@ class DatabaseConnection(BaseConnection):
     def cursor_kwargs(self):
         return dict(cursor_factory=pg.extras.RealDictCursor)
 
-    @retry(
-        (
-            pg.InterfaceError,
-            pg.extensions.TransactionRollbackError,
-            pg.extensions.QueryCanceledError
-            ),
-        tries=3)
-    def execute(self, *query):
-        return super(DatabaseConnection, self).execute(*query)
+    @memoized_property
+    def pool(self):
+        if self.pool_config is None:
+            return None
+        pool_name = self.pool_config.get("pool", None)
+        if pool_name is None:
+            return None
+        pool = getattr(sys.modules[__name__], pool_name)
+        min_connections = self.pool_config.get("min_connections", 2)
+        max_connections = self.pool_config.get("max_connections", min_connections * 2)
+        return pool(min_connections, max_connections, **self.connect_kwargs)
+
+    @retry((pg.InterfaceError, pg.extensions.TransactionRollbackError, pg.extensions.QueryCanceledError), tries=3)
+    def execute(self, *query, write=False, **kwargs):
+        return super(DatabaseConnection, self).execute(*query, write=write, **kwargs)

jardin/database/drivers/sf.py
@@ -20,7 +20,8 @@ class Lexicon(PGLexicon):
 
     @staticmethod
     def standardize_interpolators(sql, params):
-        sql, params = super(Lexicon, Lexicon).standardize_interpolators(sql, params)
+        sql, params = super(
+            Lexicon, Lexicon).standardize_interpolators(sql, params)
         param_names = re.findall(r'\%\((\w+)\)s', sql)
         if len(param_names):
             sql = re.sub(r'\%\(\w+\)s', '%s', sql)
@@ -45,7 +46,7 @@ class DatabaseConnection(BaseConnection):
             account=self.db_config.account,
             database=self.db_config.database,
             schema=self.db_config.schema
-            )
+        )
         if 'warehouse' in dir(self.db_config):
             kwargs['warehouse'] = self.db_config.warehouse
         if 'authenticator' in dir(self.db_config):
@@ -55,9 +56,10 @@ class DatabaseConnection(BaseConnection):
         return kwargs
 
     @retry(sf.OperationalError, tries=3)
-    def connect(self):
-        return super(DatabaseConnection, self).connect()
+    def get_connection(self):
+        return super(DatabaseConnection, self).get_connection()
 
     @retry(sf.InterfaceError, tries=3)
-    def execute(self, *query):
-        return super(DatabaseConnection, self).execute(*query)
+    def execute(self, *query, write=False, **kwargs):
+        return super(DatabaseConnection, self).execute(*query, write=False, **kwargs)
+

commit 7a71224e13c76ce59242f17f156dc95a45ebacd4

Re-raise general exceptions
This appears to have been an accidental regression in
commit 80671e2.

jardin/database/base.py
@@ -27,8 +27,9 @@ class BaseConnection(object):
         except self.DRIVER.InterfaceError:
             self._connection = None
             raise
-        except Exception as e:
+        except Exception:
             self.rollback()
+            raise
         finally:
             if self.pool is not None and self.autocommit:
                 key = threading.current_thread().ident