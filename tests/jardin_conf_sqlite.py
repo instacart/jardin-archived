ab899612e4f8e24dbbe0c53b1da93ed9ccedb546
Keith Lazuka <keith.lazuka@instacart.com>
2020-12-16 12:01:40 -0500

    Add tests for `jardin.reset_session()`

--- tests/jardin_conf_sqlite.py
+++ tests/jardin_conf_sqlite.py
@@ -1,7 +1,9 @@
 import logging
 
 DATABASES = {
-    'jardin_test': 'sqlite://localhost/jardin_test.db'
+    'jardin_test': 'sqlite://localhost/jardin_test.db',
+    # a db with multiple replica URLs. The 1st url works, but the 2nd is intentionally bogus.
+    'multi_url_test': 'sqlite://localhost/jardin_test.db sqlite://localhost/bogus.db',
 }
 
 LOG_LEVEL = logging.INFO
