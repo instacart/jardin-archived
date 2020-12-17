04e6e92e9215eb7aa452c1d888610d2d914db8ae
Keith Lazuka <keith.lazuka@instacart.com>
2020-12-16 19:38:43 -0500

    Fix broken test

--- tests/jardin_conf_pg.py
+++ tests/jardin_conf_pg.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-PGPORT = os.environ.get("PGPORT", 5432)
+PGPORT = int(os.environ.get("PGPORT", 5432))
 
 DATABASES = {
     'jardin_test': f'postgres://postgres:@localhost:{PGPORT}/jardin_test',
