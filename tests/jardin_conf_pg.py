a58539d0824a88272c47d42c7ac6f5b47ad44b59
Keith Lazuka <keith.lazuka@instacart.com>
2021-10-11 14:18:37 -0400

    Explicitly provide a password in the connection string

--- tests/jardin_conf_pg.py
+++ tests/jardin_conf_pg.py
@@ -1,10 +1,11 @@
 import logging
 import os
 
+PGPASSWORD = os.environ.get("PGPASSWORD", "")
 PGPORT = int(os.environ.get("PGPORT", 5432))
 
 DATABASES = {
-    'jardin_test': f'postgres://postgres:@localhost:{PGPORT}/jardin_test',
+    'jardin_test': f'postgres://postgres:{PGPASSWORD}@localhost:{PGPORT}/jardin_test',
     'other_test_dict_config': {
         'username': 'test',
         'password': 'test',
@@ -14,7 +15,7 @@ DATABASES = {
     },
 
     # a db with multiple replica URLs. The 1st url refers to an active server. The 2nd url will fail to connect.
-    'multi_url_test': f'postgres://postgres:@localhost:{PGPORT}/jardin_test postgres://postgres:@localhost:{PGPORT+1}/jardin_test',
+    'multi_url_test': f'postgres://postgres:{PGPASSWORD}@localhost:{PGPORT}/jardin_test postgres://postgres:{PGPASSWORD}@localhost:{PGPORT+1}/jardin_test',
 }
 
 LOG_LEVEL = logging.INFO
