8f66aa8f152be8e22fdb675f912e6087d542654e
Keith Lazuka <keith.lazuka@instacart.com>
2021-10-12 15:23:21 -0400

    Add a Github action workflow (#85)

--- tests/jardin_conf_mysql.py
+++ tests/jardin_conf_mysql.py
@@ -1,10 +1,13 @@
 import logging
+import os
+
+MYSQL_PWD = os.environ.get("MYSQL_PWD", "")
 
 DATABASES = {
-    'jardin_test': 'mysql://root:@localhost:3306/jardin_test',
+    'jardin_test': f'mysql://root:{MYSQL_PWD}@localhost:3306/jardin_test',
 
     # a db with multiple replica URLs. The 1st url refers to an active server. The 2nd url will fail to connect.
-    'multi_url_test': 'mysql://root:@localhost:3306/jardin_test mysql://root:@localhost:3333/jardin_test',
+    'multi_url_test': f'mysql://root:{MYSQL_PWD}@localhost:3306/jardin_test mysql://root:{MYSQL_PWD}@localhost:3333/jardin_test',
 }
 
 CACHE = {
