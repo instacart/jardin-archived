a729874e92623ca492bf5edd45bcdc7b85b5a331
Mostafa Abdelraouf <mostafa.abdelraouf@instacart.com>
2021-10-13 00:34:30 +0200

    Some fixes

--- tests/jardin_conf_pg.py
+++ tests/jardin_conf_pg.py
@@ -5,40 +5,44 @@ os.environ["JARDIN_MAX_RETRIES"] = "3"
 os.environ["JARDIN_BACKOFF_BASE_TIME_SECONDS"] = "0"
 
 PGPORT = int(os.environ.get("PGPORT", 5432))
-base_connection = "postgres://main_user:@localhost:{port}/{db}"
+PGUSER = os.environ.get("PGUSER", 'postgres')
+PGPASS = os.environ.get("PGPASSWORD", '')
+PGDB   = os.environ.get("PGDATABASE", 'jardin_test')
+
+base_connection = "postgres://{user}:{password}@localhost:{port}/{db}"
 DATABASES = {
-    'jardin_test': base_connection.format(port=PGPORT, db="main_db"),
+    'jardin_test': base_connection.format(port=PGPORT, db=PGDB, user=PGUSER, password=PGPASS),
     'other_test_dict_config': {
-        'username': 'main_user',
-        'password': '',
-        'database': 'main_db',
+        'username': 'test',
+        'password': 'test',
+        'database': 'jardin_test',
         'host': 'localhost',
         'port': 1234
     },
 
     # a db with multiple replica URLs. The 1st url refers to an active server. The 2nd url will fail to connect.
-    'multi_url_test': f'postgres://main_user:@localhost:{PGPORT}/main_db postgres://main_user:@localhost:{PGPORT+1}/main_db',
+    'multi_url_test': f'postgres://{PGUSER}:{PGPASS}@localhost:{PGPORT}/{PGDB} postgres://{PGUSER}:{PGPORT}@localhost:{PGPORT+1}/{PGDB}',
 
     'some_bad': " ".join([
-      base_connection.format(port=PGPORT+0, db="main_db"),
-      base_connection.format(port=PGPORT+1, db="second"),
-      base_connection.format(port=PGPORT+2, db="third"),
+        base_connection.format(port=PGPORT+0, user=PGUSER, password=PGPASS, db=PGDB),
+        base_connection.format(port=PGPORT+1, user=PGUSER, password=PGPASS, db=PGDB + "_second"),
+        base_connection.format(port=PGPORT+2, user=PGUSER, password=PGPASS, db=PGDB + "_thrid")
     ]),
 
     'all_bad': " ".join([
-      base_connection.format(port=PGPORT+1, db="first"),
-      base_connection.format(port=PGPORT+2, db="second"),
-      base_connection.format(port=PGPORT+3, db="third"),
+        base_connection.format(port=PGPORT+1, user=PGUSER, password=PGPASS, db=PGDB + "_first"),
+        base_connection.format(port=PGPORT+2, user=PGUSER, password=PGPASS, db=PGDB + "_second"),
+        base_connection.format(port=PGPORT+3, user=PGUSER, password=PGPASS, db=PGDB + "_thrid")
     ]),
 
     'all_good': " ".join([
-      base_connection.format(port=PGPORT, db="main_db"),
-      base_connection.format(port=PGPORT, db="main_db"),
-      base_connection.format(port=PGPORT, db="main_db"),
+        base_connection.format(port=PGPORT, user=PGUSER, password=PGPASS, db=PGDB),
+        base_connection.format(port=PGPORT, user=PGUSER, password=PGPASS, db=PGDB),
+        base_connection.format(port=PGPORT, user=PGUSER, password=PGPASS, db=PGDB)
     ]),
 
-    'good_primary': base_connection.format(port=PGPORT+0, db="main_db"),
-    'bad_primary':  base_connection.format(port=PGPORT+1, db="main_db"),
+    'good_primary': base_connection.format(port=PGPORT+0, user=PGUSER, password=PGPASS, db=PGDB),
+    'bad_primary':  base_connection.format(port=PGPORT+1, user=PGUSER, password=PGPASS, db=PGDB)
 }
 
 LOG_LEVEL = logging.INFO
